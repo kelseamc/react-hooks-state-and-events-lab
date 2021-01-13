a71e6f994db7fdaebbdaf0bc2551cfd0803457b3
kelseamc <kelsea.mcall@gmail.com>
2021-01-13 15:56:36 -0500

    commit tree 8ad1611025974bf561c8c9978f3a49a8a8a23411

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,19 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
-
+  const [darkMode, setDarkMode] = useState(false)
   // this data will be passed down to the ShoppingList as a prop
-  console.log(items);
+  // console.log(items);
 
+  function handleDarkClick(){
+    setDarkMode((darkMode) => !darkMode)
+    
+  }
   return (
-    <div className={"App " + (false ? "dark" : "light")}>
+    <div className={"App " + (darkMode ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={() => handleDarkClick()}>{darkMode ? "Light Mode" : "Dark Mode"}</button>
       </header>
       <ShoppingList items={items} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [cartItem, setCartItem] = useState("")
+  
+  function handleAddItem(){
+    setCartItem("in-cart")
+  }
+
+
   return (
-    <li className="">
+    <li className={cartItem}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddItem}>Add to Cart</button>
     </li>
   );
 }
